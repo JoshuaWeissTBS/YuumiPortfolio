1fe4da6087162afd7d74eb30184bfb7def448b20
Joshua Weiss <joshua.weiss@hotmail.com>
2023-08-15 15:17:24 -0700

    'collect' method of LostChapter that turns the lost chapter transparent and blue, dimming lights and animations

--- assets/objects/lost_chapter.gd
+++ assets/objects/lost_chapter.gd
@@ -1,11 +1,26 @@
 extends Node3D
 
+@export var collected_tres: StandardMaterial3D
+@export var uncollected_tres: StandardMaterial3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$AnimationPlayer.play("hovering")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func collect():
+	# TODO: play sfx
+	# TOOD: return data required to open portfolio info
+	
+	$AnimationTree.set("parameters/Collected/transition_request", "collected")
+	
+	# Override materials
+	$Node3D/Plane.material_override = collected_tres
+	$Node3D/Plane_001.material_override = collected_tres
+	$Node3D/Plane_002.material_override = collected_tres
+	$Node3D/Plane_003.material_override = collected_tres
+	
+	# Hide outline
+	$Node3D/Plane/MeshInstance3D.visible = false
+	$Node3D/Plane_001/MeshInstance3D.visible = false
+	$Node3D/Plane_002/MeshInstance3D.visible = false
+	$Node3D/Plane_003/MeshInstance3D.visible = false
+	
+	# Stop particles and dimmed light
+	$Node3D/GPUParticles3D.emitting = false
+	$Node3D/OmniLight3D.omni_range = 2.0
