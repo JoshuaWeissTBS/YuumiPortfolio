f06e2ec8418a850e48b214e4d0916350396d50ed
Joshua Weiss <44821795+JoshuaWeissTBS@users.noreply.github.com>
2023-08-19 20:56:14 -0700

    Collect lost chapter & open book (#9)
    * 'collect' method of LostChapter that turns the lost chapter transparent and blue, dimming lights and animations
    
    * added PlayerInteractionRange in LostChapter, upon player entry collects LostChapter
    
    * imported animated book, shuffled around file structure, stepping on lost chapter plays open book animation
    
    * begun creating portfolio info scene, collecting lost book called by player upon entry, stops player movement, and returns profile info scene from lost chapter
    
    * Added more lost chapters to the area
    
    * updated git ignore and added font
    
    * created Sprite3D for book of thresholds that displays project info in lost chapters.
    
    * Space to continue text appears when you open a chapter, pressing space closes it
    
    * keep text centered and anchored to bottom

---
+++ assets/objects/lost_chapter/lost_chapter.gd
@@ -0,0 +1,34 @@
+extends Node3D
+
+@export var collected_tres: StandardMaterial3D
+@export var uncollected_tres: StandardMaterial3D
+@export var portfolio_info: CompressedTexture2D
+var collected: bool = false # TODO: fetch collected state from local storage/cookies
+
+
+func collect() -> CompressedTexture2D:
+	# TODO: play sfx
+	$"../Camera3D/BookOfThresholds/AnimationTree".set("parameters/Transition/transition_request", "open")
+	$AnimationTree.set("parameters/Collected/transition_request", "collected")
+	
+	# Override materials
+	$Node3D/Plane.material_override = collected_tres
+	$Node3D/Plane_001.material_override = collected_tres
+	$Node3D/Plane_002.material_override = collected_tres
+	$Node3D/Plane_003.material_override = collected_tres
+	
+	# Hide outline
+	$Node3D/Plane/MeshInstance3D.visible = false
+	$Node3D/Plane_001/MeshInstance3D.visible = false
+	$Node3D/Plane_002/MeshInstance3D.visible = false
+	$Node3D/Plane_003/MeshInstance3D.visible = false
+	
+	# Stop particles and dimmed light
+	$Node3D/GPUParticles3D.emitting = false
+	$Node3D/OmniLight3D.omni_range = 2.0
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property($"../CanvasLayer/Label", "modulate", Color.WHITE, 1)
+	$"../Camera3D/BookOfThresholds".set_book_content_texture(portfolio_info)
+	
+	return portfolio_info
