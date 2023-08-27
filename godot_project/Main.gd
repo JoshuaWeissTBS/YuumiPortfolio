70a0c92e2bf44aa4fd5f8660967e62e5366f1f10
Joshua Weiss <joshua.weiss@hotmail.com>
2023-08-27 16:56:41 -0700

    added completed page collection VO

--- godot_project/Main.gd
+++ godot_project/Main.gd
@@ -1,6 +1,6 @@
 extends Node3D
 var collected_chapters = 0 # TODO: replace with cached
-const MAX_CHAPTERS = 8 # TODO: replace with real amount
+const MAX_CHAPTERS = 4 # TODO: replace with real amount
 @onready var unplayed_collect_voicelines = [$Collect1, $Collect2, $Collect3]
 
 # Called when the node enters the scene tree for the first time.
@@ -9,6 +9,8 @@ func _ready():
 
 func increment_collected_chapters():
 	collected_chapters += 1
+	if (collected_chapters == MAX_CHAPTERS):
+		$CompletedPageCollection.play()
 	$CanvasLayer/LostChapterCollectionControl/LostChapterCollectionLabel.text = str(collected_chapters) + "/" + str(MAX_CHAPTERS)
 
 func play_collected_vo():
