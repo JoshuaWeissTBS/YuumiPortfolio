2680d15f670150e42908130e48772bfcaaf0bba8
Joshua Weiss <joshua.weiss@hotmail.com>
2023-08-27 17:48:58 -0700

    added closing book VO

--- godot_project/assets/objects/book_of_thresholds/book_of_thresholds.gd
+++ godot_project/assets/objects/book_of_thresholds/book_of_thresholds.gd
@@ -7,3 +7,13 @@ func set_book_content_texture(texture: CompressedTexture2D):
 func close_book():
 	$AnimationTree.set("parameters/Transition/transition_request", "close")
 	player_controller.in_book = false
+	_play_close_book_vo()
+	
+
+var rng = RandomNumberGenerator.new()
+func _play_close_book_vo():
+	await get_tree().create_timer(0.5).timeout
+	if $"../../Collect1".playing or $"../../Collect2".playing or $"../../Collect3".playing:
+		return
+	if rng.randi_range(0, 2) > 0:
+		$CloseBookVO.play()
